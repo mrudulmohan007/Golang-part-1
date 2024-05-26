0d15a2d161e5ac1e316d4cfa57b4195250f97fc2
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-05-26 23:35:51 +0530

    slices in go

--- arrays-and-slices/main.go
+++ arrays-and-slices/main.go
@@ -57,4 +57,63 @@ func main() {
 
 	// "-----------SLICE-------------------------------------------"
 
+	p := []int{1, 2, 3}
+	q := p
+	q[1] = 6
+	fmt.Println(p)
+	fmt.Println(q)
+	fmt.Printf("Length: %v\n", len(p))
+	fmt.Printf("Capacity: %v\n", cap(p))
+
+	fmt.Println("-----------SLICE-------------------------------------------")
+
+	//slice syntax
+
+	x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	all_elements := x[:]                        //slice of all elements
+	slice_from_four_to_end := x[3:]             //slice from 4th element to end
+	slice_first_6_elements := x[:6]             //slice first 6 elements
+	slice_fourth_fifth_sixth_elements := x[3:6] //slice 4th,5th,6th elements
+
+	fmt.Println(all_elements)
+	fmt.Println(slice_from_four_to_end)
+	fmt.Println(slice_first_6_elements)
+	fmt.Println(slice_fourth_fifth_sixth_elements)
+
+	fmt.Println("-----------SLICE-------------------------------------------")
+
+	//slice eg:
+	u := []int{}
+	fmt.Println(u)
+	fmt.Printf("Length: %v\n", len(u))
+	fmt.Printf("Capacity: %v\n", cap(u))
+	u = append(u, 1)
+	fmt.Println(u)
+	fmt.Printf("Length: %v\n", len(u))
+	fmt.Printf("Capacity: %v\n", cap(u))
+
+	u = append(u, []int{2, 3, 4, 5}...) // here ... is added to spread the slice to individual arguments
+	//otherwise it will give u error
+	fmt.Println(u)
+	fmt.Printf("Length: %v\n", len(u))
+	fmt.Printf("Capacity: %v\n", cap(u))
+
+	fmt.Println("-----------SLICE-------------------------------------------")
+
+	//another slice eg: print all elements of a slice
+
+	i := []int{1, 2, 3, 4, 5}
+	j := i[:]
+	fmt.Println(j)
+	//remove last element from a slice
+	k := i[:len(i)-1]
+	fmt.Println(k)
+
+	//to remove an element from middle
+
+	m := []int{1, 2, 3, 4, 5}
+	n := append(m[:2], m[3:]...)
+
+	fmt.Println(n)
+
 }
