9a94166a94e2b8e630a9ca0d07095382cb9e9c12
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-06-02 13:54:45 +0530

    defer in go

---
+++ defer/main.go
@@ -0,0 +1,28 @@
+//defer statements doesn't execute at the end of the main function
+//instead it executes after the main function but before the main function returned.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Start")
+	defer fmt.Println("Middle")
+	fmt.Println("End") //output will be Start, End, Middle
+
+	//in this eg if all these statements were defer go will follow LIFO(last in first out)
+	// defer fmt.Println("Start")
+	// defer fmt.Println("Middle")
+	//defer fmt.Println("End")
+
+	//the output will be: End,Middle,Start
+
+	//another exmaple
+
+	a := "start"
+	defer fmt.Println(a)
+	a = "end"
+
+	//output will be start (because defer will take the argument when the defer is called)
+
+}
