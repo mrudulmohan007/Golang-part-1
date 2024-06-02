e45b2cd5426373c96966c2a8f3c4a2bda4f3c19e
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-06-02 20:48:25 +0530

    panic in go

---
+++ panic/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 1, 0
+	ans := a / b
+	fmt.Println(ans)
+
+	//Output will be panic: runtime error
+
+	//eg1
+
+	fmt.Println("Start")
+	panic("Something bad happened")
+	fmt.Println("end") //output will be Start and a error saying: Something bad happened
+
+	//eg2
+	fmt.Println("Start")
+	defer fmt.Println("This was deferred")
+	panic("Something bad happened")
+	fmt.Println("end") //Here output will be start and this was referred and panic: something bad happened
+	//its because panic statements execute after deferred statements
+	//so the order of execution is main function,deferred statements and then panic
+	//and then finally return value
+
+}
