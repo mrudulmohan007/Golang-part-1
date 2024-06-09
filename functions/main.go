6126ef21547352af2c50040bfbacdc1c05997b56
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-06-09 20:15:29 +0530

    anonymous functions

--- functions/main.go
+++ functions/main.go
@@ -102,18 +102,25 @@ import "fmt"
 
 //Eg-7 multiple return values
 
-func main() {
-	d, err := divide(5.0, 0.0)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(d)
+// func main() {
+// 	d, err := divide(5.0, 0.0)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	fmt.Println(d)
 
-}
-func divide(a, b float64) (float64, error) {
-	if b == 0.0 {
-		return 0.0, fmt.Errorf("cannot divide by zero")
-	}
-	return a / b, nil
+// }
+// func divide(a, b float64) (float64, error) {
+// 	if b == 0.0 {
+// 		return 0.0, fmt.Errorf("cannot divide by zero")
+// 	}
+// 	return a / b, nil
+// }
+
+//eg-8-Anonymous function-------------------------------
+func main() {
+	func() {
+		fmt.Println("Hello Go!")
+	}()
 }
