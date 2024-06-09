a267e4831181c4f5230fef20cd40e4f6bbeaa327
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-06-09 20:00:37 +0530

    fn with multiple return values

--- functions/main.go
+++ functions/main.go
@@ -42,15 +42,78 @@ import "fmt"
 // }
 
 //eg-4-variadic parameter ----->> Variadic Function: A function that can take a variable
-//number of arguments (values ...int).
+//number of arguments (values ...int)
+
+// func main() {
+// 	sum("The sum is ", 1, 2, 3, 4, 5)
+// }
+// func sum(msg string, values ...int) {
+// 	fmt.Println(values)
+// 	result := 0
+// 	for _, v := range values {
+// 		result = result + v
+// 	}
+// 	fmt.Println(msg, result)
+// }
+
+//eg-5- fn with arguments
+
+// func main() {
+// 	s := sum(1, 2, 3, 4, 5)
+// 	fmt.Println("The sum is ", s)
+// }
+// func sum(values ...int) int {
+// 	fmt.Println(values)
+// 	result := 0
+// 	for _, v := range values {
+// 		result = result + v
+// 	}
+// 	return result
+// }
+
+//OR--ANOTHER WAY TO WRITE Eg-5---------------------------------------------------
+
+// func main() {
+// 	s := sum(1, 2, 3, 4, 5)
+// 	fmt.Println("The sum is ", s)
+// }
+// func sum(values ...int) (result int) {
+// 	fmt.Println(values)
+// 	for _, v := range values {
+// 		result = result + v
+// 	}
+// 	return
+// }
+
+//eg-6- fn which return local variable as a pointer-------------------------------------------------
+
+// func main() {
+// 	s := sum(1, 2, 3, 4, 5)
+// 	fmt.Println("The sum is ", *s)
+// }
+// func sum(values ...int) *int {   //The function sum returns a pointer to an integer (*int).
+// 	fmt.Println(values)
+// 	result := 0
+// 	for _, v := range values {
+// 		result = result + v
+// 	}
+// 	return &result
+// }
+
+//Eg-7 multiple return values
+
 func main() {
-	sum("The sum is ", 1, 2, 3, 4, 5)
-}
-func sum(msg string, values ...int) {
-	fmt.Println(values)
-	result := 0
-	for _, v := range values {
-		result = result + v
+	d, err := divide(5.0, 0.0)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	fmt.Println(msg, result)
+	fmt.Println(d)
+
+}
+func divide(a, b float64) (float64, error) {
+	if b == 0.0 {
+		return 0.0, fmt.Errorf("cannot divide by zero")
+	}
+	return a / b, nil
 }
