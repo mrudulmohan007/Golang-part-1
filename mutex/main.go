1c1a23891bfe3390f8a621f35735a27d2e067784
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-06-21 15:19:49 +0530

    mutex in go

---
+++ mutex/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var signals = []string{"test"}
+var wg sync.WaitGroup
+var mut sync.Mutex
+
+func main() {
+	websitelist := []string{
+
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://youtube.com",
+	}
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println(signals)
+
+}
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
+	} else {
+		mut.Lock()
+		signals = append(signals, endpoint)
+		mut.Unlock()
+		fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
+	}
+
+}
