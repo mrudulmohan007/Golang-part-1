b892e62e6bb753bb421badf4a15e08fdd1d4a7ce
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-06-28 23:54:05 +0530

    channels in go commit 2

--- channels/main.go
+++ channels/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	fmt.Println("Channels in Golang")
+	myCh := make(chan int, 2) //buffer channel
+	wg := &sync.WaitGroup{}
+	// myCh <- 5 //push a value into myCh
+	// fmt.Println(<-myCh)
+
+	wg.Add(2)
+	go func(ch chan int, wg *sync.WaitGroup) {
+		fmt.Println(<-myCh)
+
+		defer wg.Done()
+	}(myCh, wg)
+
+	go func(ch chan int, wg *sync.WaitGroup) {
+		myCh <- 5
+		myCh <- 7
+		close(myCh) //closing the channel
+		defer wg.Done()
+	}(myCh, wg)
+	wg.Wait()
 }
