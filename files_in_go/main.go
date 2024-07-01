b01ddde8fc8ddc0465b7a9a3b89a622e1116ac3b
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-07-02 01:45:41 +0530

    files in go commit 2

--- files_in_go/main.go
+++ files_in_go/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
 	fmt.Println("Files in Go")
+	content := "This is a test file that should be put in a text file"
+	//we have to put this content in a text file
+	file, err := os.Create("./test.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	checkNilError(err)
+	length, err := io.WriteString(file, content)
+	checkNilError(err)
+	fmt.Println("Length is: ", length)
+	defer file.Close()
+	readFile("./test.txt")
+}
+
+// fn to read the file
+func readFile(filename string) {
+	databyte, err := os.ReadFile(filename)
+	checkNilError(err)
+	fmt.Println("Text data inside the file is \n", string(databyte))
+}
+
+//fn to check error
+
+func checkNilError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
